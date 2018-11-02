563dc4565f289698d5dfc82bd12dfc8feda9d1f2
Novi1st <44486324+Novi1st@users.noreply.github.com>
2018-11-02 22:44:39 +0700

    VERSI2
    02/11/2018

--- TheMovieDBApp-master/app/src/main/java/kashish/com/interfaces/OnReviewReadMoreClickListener.kt
+++
@@ -1,9 +0,0 @@
-package kashish.com.interfaces
-import kashish.com.models.MovieReview
-
-/**
- * Created by Kashish on 04-08-2018.
- */
-interface OnReviewReadMoreClickListener {
-    fun onReviewReadMoreClickListener(review: MovieReview)
-}
